df946cbff9c0f7c29db432d0db0d54e9eac9aaac
Ali Torabi <ali-torabian@outlook.com>
2023-08-19 22:45:52 +0200

    Updates

--- _next/static/lwmaozEG5gbmmVNtfWJkK/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f03beedc525bd78d.css","static/chunks/pages/index-ffececbeb77077ca.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
