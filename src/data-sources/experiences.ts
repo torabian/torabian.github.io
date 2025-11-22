481c751e2c73fbe422af8a57dd834f1f4314b857
Ali <Ali@ali.com>
2025-11-22 16:38:03 +0100

    Update the experience data

--- src/data-sources/experiences.ts
+++ src/data-sources/experiences.ts
@@ -5,30 +5,8 @@ export const experiences: Experience[] = [
     "category": "Project",
     "content": "",
     "date": "",
-    "description": "In this video, I am explaining my front-end experience. I have highlighted web front-end only in this video.\r",
-    "featured": true,
-    "id": "frontend-experience",
-    "videoUrl": "https://www.youtube.com/embed/kwFtGI6fJQk?si=9Ot3rdoENp7bPJaQ",
-    "tags": [],
-    "title": "Front-end experience\r"
-  },
-  {
-    "videoUrl": "https://www.youtube.com/embed/ij8R7YHt5Q8?si=5VTV15EHWauTgMpy",
-    "category": "Project",
-    "content": "",
-    "date": "",
-    "description": "Overall highlights about my backend experiences.\r",
-    "featured": true,
-    "id": "backend-experience",
-    "tags": [],
-    "title": "Backend-end experience\r"
-  },
-  {
-    "category": "Project",
-    "content": "",
-    "date": "",
     "description": "Airport kiosk app was developed with me and other colleages for checkin system of a major airline\r",
-    "featured": false,
+    "featured": true,
     "id": "airport-kiosk-app",
     "tags": [],
     "title": "Airport kiosk app\r"
@@ -38,7 +16,7 @@ export const experiences: Experience[] = [
     "content": "",
     "date": "",
     "description": "Amazing timepicker, is a unique experience in my career, which was written by me, and a friend as an open-source,\r",
-    "featured": false,
+    "featured": true,
     "id": "amazing-time-picker",
     "tags": [],
     "title": "Amazing time picker\r"
@@ -47,8 +25,18 @@ export const experiences: Experience[] = [
     "category": "Project",
     "content": "",
     "date": "",
+    "description": "In this video, I am explaining my backend-end experience. I have highlighted web backend-end only in this video.\r",
+    "featured": true,
+    "id": "backend-experience",
+    "tags": [],
+    "title": "Back-end experience\r"
+  },
+  {
+    "category": "Project",
+    "content": "",
+    "date": "",
     "description": "I have created a front-end part of a banking tool, for one of the largest investment banks in Europe. A outsourcing company \r",
-    "featured": false,
+    "featured": true,
     "id": "banking-transaction-auditing-tool",
     "tags": [],
     "title": "Banking transaction auditing tool.\r"
@@ -58,18 +46,27 @@ export const experiences: Experience[] = [
     "content": "",
     "date": "",
     "description": "I have been hired to create an engraving tool from scratch (kinda migrating existing one from jquery to react)\r",
-    "featured": false,
+    "featured": true,
     "id": "engraving-tool",
     "tags": [],
     "title": "Engraving tool\r"
   },
-
+  {
+    "category": "Project",
+    "content": "",
+    "date": "",
+    "description": "In this video, I am explaining my front-end experience. I have highlighted web front-end only in this video.\r",
+    "featured": true,
+    "id": "frontend-experience",
+    "tags": [],
+    "title": "Front-end experience\r"
+  },
   {
     "category": "Project",
     "content": "",
     "date": "",
     "description": "Working for a German client, I've participated in development of their application, which would\r",
-    "featured": false,
+    "featured": true,
     "id": "heatpump-and-gas-boiler-controller",
     "tags": [],
     "title": "Heatpump and gas boiler controller application\r"
@@ -79,7 +76,7 @@ export const experiences: Experience[] = [
     "content": "",
     "date": "",
     "description": "I have started on this project for a client in Poland, and their service was to list jobs that people can do\r",
-    "featured": false,
+    "featured": true,
     "id": "job-market-search-platform",
     "tags": [],
     "title": "Job market search platform\r"
@@ -89,7 +86,7 @@ export const experiences: Experience[] = [
     "content": "",
     "date": "",
     "description": "One of my great experience in terms of mobile application, was a telephony app which would allow users\r",
-    "featured": false,
+    "featured": true,
     "id": "telephony-app",
     "tags": [],
     "title": "Telephony app\r"
