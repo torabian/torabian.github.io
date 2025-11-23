e8957c35fb2362dc5a0cca48aca65c30bd18cd70
Ali <Ali>
2025-11-23 04:27:29 +0100

    Rebuild

--- src/data-sources/experiences.ts
+++ src/data-sources/experiences.ts
@@ -5,90 +5,90 @@ export const experiences: Experience[] = [
     "category": "Project",
     "content": "",
     "date": "",
-    "description": "Airport kiosk app was developed with me and other colleages for checkin system of a major airline\r",
+    "description": "Airport kiosk app was developed with me and other colleages for checkin system of a major airline",
     "featured": true,
     "id": "airport-kiosk-app",
     "tags": [],
-    "title": "Airport kiosk app\r"
+    "title": "Airport kiosk app"
   },
   {
     "category": "Project",
     "content": "",
     "date": "",
-    "description": "Amazing timepicker, is a unique experience in my career, which was written by me, and a friend as an open-source,\r",
+    "description": "Amazing timepicker, is a unique experience in my career, which was written by me, and a friend as an open-source,",
     "featured": true,
     "id": "amazing-time-picker",
     "tags": [],
-    "title": "Amazing time picker\r"
+    "title": "Amazing time picker"
   },
   {
     "category": "Project",
     "content": "",
     "date": "",
-    "description": "In this video, I am explaining my backend-end experience. I have highlighted web backend-end only in this video.\r",
+    "description": "In this video, I am explaining my backend-end experience. I have highlighted web backend-end only in this video.",
     "featured": true,
     "id": "backend-experience",
     "tags": [],
-    "title": "Back-end experience\r"
+    "title": "Back-end experience"
   },
   {
     "category": "Project",
     "content": "",
     "date": "",
-    "description": "I have created a front-end part of a banking tool, for one of the largest investment banks in Europe. A outsourcing company \r",
+    "description": "I have created a front-end part of a banking tool, for one of the largest investment banks in Europe. A outsourcing company ",
     "featured": true,
     "id": "banking-transaction-auditing-tool",
     "tags": [],
-    "title": "Banking transaction auditing tool.\r"
+    "title": "Banking transaction auditing tool."
   },
   {
     "category": "Project",
     "content": "",
     "date": "",
-    "description": "I have been hired to create an engraving tool from scratch (kinda migrating existing one from jquery to react)\r",
+    "description": "I have been hired to create an engraving tool from scratch (kinda migrating existing one from jquery to react)",
     "featured": true,
     "id": "engraving-tool",
     "tags": [],
-    "title": "Engraving tool\r"
+    "title": "Engraving tool"
   },
   {
     "category": "Project",
     "content": "",
     "date": "",
-    "description": "In this video, I am explaining my front-end experience. I have highlighted web front-end only in this video.\r",
+    "description": "In this video, I am explaining my front-end experience. I have highlighted web front-end only in this video.",
     "featured": true,
     "id": "frontend-experience",
     "tags": [],
-    "title": "Front-end experience\r"
+    "title": "Front-end experience"
   },
   {
     "category": "Project",
     "content": "",
     "date": "",
-    "description": "Working for a German client, I've participated in development of their application, which would\r",
+    "description": "Working for a German client, I've participated in development of their application, which would",
     "featured": true,
     "id": "heatpump-and-gas-boiler-controller",
     "tags": [],
-    "title": "Heatpump and gas boiler controller application\r"
+    "title": "Heatpump and gas boiler controller application"
   },
   {
     "category": "Project",
     "content": "",
     "date": "",
-    "description": "I have started on this project for a client in Poland, and their service was to list jobs that people can do\r",
+    "description": "I have started on this project for a client in Poland, and their service was to list jobs that people can do",
     "featured": true,
     "id": "job-market-search-platform",
     "tags": [],
-    "title": "Job market search platform\r"
+    "title": "Job market search platform"
   },
   {
     "category": "Project",
     "content": "",
     "date": "",
-    "description": "One of my great experience in terms of mobile application, was a telephony app which would allow users\r",
+    "description": "One of my great experience in terms of mobile application, was a telephony app which would allow users",
     "featured": true,
     "id": "telephony-app",
     "tags": [],
-    "title": "Telephony app\r"
+    "title": "Telephony app"
   }
 ];
